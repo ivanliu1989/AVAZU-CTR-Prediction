540023b49d4c48bbc9794be12c18a89c2cf44577
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-01-06 07:28:43 +1100

    update

--- R/ensemble_models.R
+++ R/ensemble_models.R
@@ -5,7 +5,7 @@ gc(); rm(list=ls());
 options(scipen=888)
 require(data.table)
 pred_1 <- data.frame(fread('submission.csv'))
-pred_2 <- data.frame(fread('submission_0391171.csv'))
+pred_2 <- data.frame(fread('submission_0391103.csv'))
 
 identical(pred_1$id, pred_2$id)
 
@@ -18,4 +18,6 @@ head(new_pred);head(pred_1);head(pred_2)
 write.table(new_pred, file='submission_py_vw.csv',sep = ',', row.names = F)
 
 pred_3 <- data.frame(fread('submission_py_vw.csv'))
-head(pred_3)
+head(pred_3,100)
+
+class(pred_3[,2])
