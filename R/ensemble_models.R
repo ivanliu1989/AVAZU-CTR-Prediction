d52af0b1da7f3e5ef61a98417bd86bbe9d23d267
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-01-05 22:39:46 +1100

    vw

--- R/ensemble_models.R
+++ R/ensemble_models.R
@@ -2,17 +2,20 @@ setwd('/Users/ivan/Work_directory/VAZU/')
 setwd('H:/Machine_Learning/VAZU/')
 gc(); rm(list=ls());
 
-pred_1 <- read.csv('submission.csv')
-pred_2 <- read.csv('submission_0391171.csv')
+options(scipen=888)
+require(data.table)
+pred_1 <- data.frame(fread('submission.csv'))
+pred_2 <- data.frame(fread('submission_0391171.csv'))
 
 identical(pred_1$id, pred_2$id)
 
-pred_ensemble <- pred_1
-pred_ensemble$click <- 0.5 * pred_1$click + 0.5 * pred_2$click
+pred_ensemble <- 0.5 * pred_1[,2] + 0.5 * pred_2[,2]
+new_pred <- cbind.data.frame(id = pred_1[,1], click = pred_ensemble)
 
-head(pred_ensemble);head(pred_1);head(pred_2)
+identical(new_pred$id, pred_2$id)
+head(new_pred);head(pred_1);head(pred_2)
 
-write.csv(pred_ensemble, file='submission_py_vw.csv',quote = F, row.names = F)
+write.table(new_pred, file='submission_py_vw.csv',sep = ',', row.names = F)
 
-pred_3 <- read.csv('submission_py_vw.csv')
+pred_3 <- data.frame(fread('submission_py_vw.csv'))
 head(pred_3)
--- R/preProcess.R
+++ R/preProcess.R
@@ -4,3 +4,26 @@ require(data.table); require(caret)
 
 train <- as.data.frame(fread('data/train.csv',stringsAsFactors = F))
 test <- as.data.frame(fread('data/test.csv',stringsAsFactors = F))
+
+head(train); head(test)
+
+train_id <- train[,1]
+train_label <- train[,2]
+test_id <- test[,1]
+
+train_data <- train[,-c(1,2)]
+test_data <- test[,-1]
+
+rm(train);rm(test)
+head(train_data);head(test_data)
+
+comb_dt <- rbind(train_data,test_data)
+rm(train_data);rm(test_data);gc()
+
+dim(comb_dt)
+### hash tricks ###
+install.packages('FeatureHashing')
+
+### feature engineering ###
+head(comb_dt)
+comb_dt$date <- substr(comb_dt$hour,1,6)
