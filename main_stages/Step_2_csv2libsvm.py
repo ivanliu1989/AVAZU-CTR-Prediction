768ea1c6193f7ba7a9fb4bf2811016f2b4c358bb
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-01-06 16:05:09 +1100

    vw

--- main_stages/Step_2_csv2libsvm.py
+++ main_stages/Step_2_csv2libsvm.py
@@ -9,46 +9,42 @@ import numpy as np
 from datetime import datetime
 from csv import DictReader
 
-# --- Transform func --- #
-def construct_line(line):
-    new_line = []
-    
-    if line['click'] == '0':
-        label = "0"
-    elif line['click'] == '1':
-        label = "1"
-    else:
-        break
-    
-    new_line.append(label)
-    
-    for i, item in enumerate(line):
-        if item == '' or float( item ) == 0.0:
-            continue
-        new_item = "%s:%s" % ( i + 1, item )
-        new_line.append( new_item )
-    new_line = " ".join( new_line )
-    new_line += "\n"
-    return new_line
-
 
 # --- Main transformation --- #
 input_file = 'data/train_df.csv'
-output_file = 'data/libsvm_train.txt'
+output_file = 'xgboost/libsvm_train.txt'
 
 start = datetime.now()
 with open(output_file,"wb") as outfile:
     for t, row in enumerate(DictReader(open(input_file))):
         
+        new_line = []
+    
+        if row[' click'] == '0':
+            label = 0
+        elif row[' click'] == '1':
+            label = 1
+        else:
+            break
         
+        new_line.append(label)
         
-        new_line = construct_line(row)
+        for i, item in row.items():
+            index = hash(item)
+            if index == '' or float(index) == 0.0 or i in ["id"," click"]:
+                continue
+            new_item = "%s:%s" % ( i+1, index )
+            new_line.append( new_item )
+            new_line = " ".join( new_line )
+            new_line += "\n"
+        
+        outfile.write(new_line)
         
         if t % 100000 == 0:
             print("%s\t%s"%(t, str(datetime.now() - start)))
             
-        outfile.write(new_line)
-    
+        
+test_df = pd.read_csv('data/test_df.csv')    
     
     
     
