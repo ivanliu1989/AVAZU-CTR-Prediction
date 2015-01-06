e80b8f635d064c60bf37846383f455d24efee55f
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-01-06 16:24:23 +1100

    csv2libsvm

--- main_stages/Step_2_csv2libsvm.py
+++ main_stages/Step_2_csv2libsvm.py
@@ -13,6 +13,7 @@ from csv import DictReader
 # --- Main transformation --- #
 input_file = 'data/train_df.csv'
 output_file = 'xgboost/libsvm_train.txt'
+D = 2**28  
 
 start = datetime.now()
 with open(output_file,"wb") as outfile:
@@ -21,30 +22,33 @@ with open(output_file,"wb") as outfile:
         new_line = []
     
         if row[' click'] == '0':
-            label = 0
+            label = '0'
         elif row[' click'] == '1':
-            label = 1
+            label = '1'
         else:
             break
         
         new_line.append(label)
         
-        for i, item in row.items():
-            index = hash(item)
-            if index == '' or float(index) == 0.0 or i in ["id"," click"]:
-                continue
-            new_item = "%s:%s" % ( i+1, index )
-            new_line.append( new_item )
-            new_line = " ".join( new_line )
-            new_line += "\n"
+        j = 0
         
+        for i, item in row.items():
+            
+            index = abs(hash(item)%D)
+            
+            if index == '' or float(index) == 0.0 or i in ['id',' click']:
+                continue
+            
+            new_item = "%s:%s" % ( j, index )
+            new_line.append( new_item )
+            j += 1
+        
+        new_line = " ".join( new_line )        
+        new_line += "\n"            
         outfile.write(new_line)
         
         if t % 100000 == 0:
             print("%s\t%s"%(t, str(datetime.now() - start)))
             
-        
-test_df = pd.read_csv('data/test_df.csv')    
-    
-    
-    
+
+   
