4c47889e85055db30ec7c64d72ca65cf73dcce4d
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-01-07 11:36:00 +1100

    noisy removal

--- main_stages/python/Step_1.2_noisy_removal.py
+++ main_stages/python/Step_1.2_noisy_removal.py
@@ -4,48 +4,51 @@ Created on Wed Jan  7 08:43:51 2015
 
 @author: ivan
 """
-
 from collections import Counter
 from datetime import datetime
 from csv import DictReader
 import pandas as pd
 
+##################
+# -- load data --#
+##################
 train = 'data/train_df.csv'               # path to training file
 test = 'data/test_df.csv'
-
-# -- test --#
 test_df = pd.read_csv(test)
-
-test_df.head()
-test_col = list(test_df.columns.values)
-
-test_freq = lambda x: Counter(test_df[x])
-
-for col in test_col:
-    if col in ['id','click','site_id','app_id','device_id']:
-        continue
-    print(col)
-    print(test_freq(col))
-
-# modification
-test_freq('hour')
-test_df.ix[(test_df.hour == 6),'hour'] = 25
-
-# hash('other') % (10**6)
-# hash('other') % (2**28)
-
-###############
-# -- train -- #
-###############
 train_df = pd.read_csv(train)
 
-train_df.head()
-train_col = list(train_df.columns.values)
+train_click_id = train_df['id','click']
+del train_df['id','click']
+test_click_id = test_df['id']
+del test_df['id']
 
-train_freq = lambda x: Counter(train_df[x])
+####################
+# -- combine df -- #
+####################
+train_df = train_df.append(test_df,ignore_index=True);
+# del train_df
+del test_df
 
-train_freq('click')
+######################
+# -- modification -- #
+######################
+df_col=list(train_df.columns.values)
+train_df['counts'] = train_df.groupby(['click']).transform(len)
 
-train_df[(train_df.click == 6)]['click'] = ''
+test_df.ix[(test_df.counts <= 5),'hour'] = 25 # ??
 
+# df_freq = lambda x: Counter(train_df[x])
+# df_freq('hour')
+# hash('other') % (10**6)
 
+################
+# -- output -- #
+################
+test_df = comb_df.ix[400000:,:]
+del com_df[].ix[400000:,:]
+
+pd.merge(train_click_id, com_df)
+pd.merge(test_click_id, test_df)
+
+train_click_id.to_csv('data/train_df_clean.csv',index=False)
+test_click_id.to_csv('data/test_df_clean.csv',index=False)
