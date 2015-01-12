7f0b2fde385e499e4c863a9927430d9d9e7f0d55
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-01-12 23:00:08 +1100

    ensemble

--- main_stages/python/Step_4.2_merge_ensemble_models.py
+++ main_stages/python/Step_4.2_merge_ensemble_models.py
@@ -9,27 +9,23 @@ from csv import DictReader
 import pandas as pd
 
 pred_file1 = 'submission_py_site_app_0.13_1_1_1_s5.csv'
-pred_file2 = 'submit_libFM_pred.csv'
+pred_file2 = 'submit_libFM_pred.csv'  
 
-pred_1 = {}
-for t, row in enumerate(DictReader(open(pred_file1))): # site/app
-    pred_1[row['id']] = row['click']
-    
 pred_2 = {}
 for t, row in enumerate(DictReader(open(pred_file2))): # site/app
     pred_2[row['id']] = row['click']
 
-submit1 = pd.read_csv(pred_file1)
-#pred_2[submit1['id'][1]]
-submit1['click2'] = 0
-
 start = datetime.now()
-for key,val in enumerate(submit1['id']):
-    submit1['click2'][key] = pred_2[val]
-    if key % 100000 == 0:
-            print("%s\t%s"%(key, str(datetime.now() - start)))
-    
-submit1['click'] = 0.5*submit1['click'] + 0.5*submit1['click2']
-del submit1['click2']
-
-submit1.to_csv('submit_ensemble_merge.csv',index=False)
+with open('submit_ensemble_merge.csv',"wb") as outfile:
+    outfile.write('id,click')
+    for t, row in enumerate(DictReader(open(pred_file1))):
+        
+        ID = row['id']
+        click = row['click']
+        click = 0.5*float(click) + 0.5*float(pred_2[ID])
+        
+        outfile.write('%s,%s\n' % (str(ID), str(click)))
+        if t % 100000 == 0:
+            print("%s\t%s"%(t, str(datetime.now() - start)))
+        
+        
