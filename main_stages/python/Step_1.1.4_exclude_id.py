261bcad7e6eaeedfc453e2928266a64cb7047f4c
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-01-12 15:41:32 +1100

    ex id

--- main_stages/python/Step_1.1.4_exclude_id.py
+++ main_stages/python/Step_1.1.4_exclude_id.py
@@ -19,7 +19,7 @@ test = 'data/test_df_app_smooth.csv'
 
 start = datetime.now()
 with open('data/train_df_app_smooth_ex_id.csv',"wb") as outfile:
-    outfile.write('id,click,hour,C1,banner_pos,app_domain,app_category,device_id,device_ip,device_model,device_type,device_conn_type,C14,C15,C16,C17,C18,C19,C20,C21\n')
+    outfile.write('id,click,hour,C1,banner_pos,app_domain,app_category,device_ip,device_model,device_type,device_conn_type,C14,C15,C16,C17,C18,C19,C20,C21\n')
     for t, row in enumerate(DictReader(open(train))):
         # turn hour really into hour, it was originally YYMMDDHH
         
@@ -30,7 +30,7 @@ with open('data/train_df_app_smooth_ex_id.csv',"wb") as outfile:
         banner_pos = row['banner_pos']
         app_domain = row['app_domain']
         app_category = row['app_category']
-        device_id = row['device_id']
+        #device_id = row['device_id']
         device_ip = row['device_ip']
         device_model = row['device_model']
         device_type = row['device_type']
@@ -45,7 +45,7 @@ with open('data/train_df_app_smooth_ex_id.csv',"wb") as outfile:
         C21 = row['C21']
         
 
-        outfile.write('%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n' % (str(ID), str(click),str(hour),str(C1),str(banner_pos),str(app_domain),str(app_category),str(device_id),str(device_ip),str(device_model),str(device_type),str(device_conn_type),str(C14),str(C15),str(C16),str(C17),str(C18),str(C19),str(C20),str(C21)))
+        outfile.write('%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n' % (str(ID), str(click),str(hour),str(C1),str(banner_pos),str(app_domain),str(app_category),str(device_ip),str(device_model),str(device_type),str(device_conn_type),str(C14),str(C15),str(C16),str(C17),str(C18),str(C19),str(C20),str(C21)))
         if t % 100000 == 0:
             print("%s\t%s"%(t, str(datetime.now() - start)))
         
@@ -53,7 +53,7 @@ with open('data/train_df_app_smooth_ex_id.csv',"wb") as outfile:
  # -- test data -- #
 start = datetime.now()
 with open('data/test_df_app_smooth_ex_id.csv',"wb") as outfile:
-    outfile.write('id,hour,C1,banner_pos,app_domain,app_category,device_id,device_ip,device_model,device_type,device_conn_type,C14,C15,C16,C17,C18,C19,C20,C21\n')
+    outfile.write('id,hour,C1,banner_pos,app_domain,app_category,device_ip,device_model,device_type,device_conn_type,C14,C15,C16,C17,C18,C19,C20,C21\n')
     for t, row in enumerate(DictReader(open(test))):
          
         ID = row['id']
@@ -62,7 +62,7 @@ with open('data/test_df_app_smooth_ex_id.csv',"wb") as outfile:
         banner_pos = row['banner_pos']
         app_domain = row['app_domain']
         app_category = row['app_category']
-        device_id = row['device_id']
+        #device_id = row['device_id']
         device_ip = row['device_ip']
         device_model = row['device_model']
         device_type = row['device_type']
@@ -77,7 +77,7 @@ with open('data/test_df_app_smooth_ex_id.csv',"wb") as outfile:
         C21 = row['C21']
         
             
-        outfile.write('%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n' % (str(ID),str(hour),str(C1),str(banner_pos),str(app_domain),str(app_category),str(device_id),str(device_ip),str(device_model),str(device_type),str(device_conn_type),str(C14),str(C15),str(C16),str(C17),str(C18),str(C19),str(C20),str(C21)))
+        outfile.write('%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n' % (str(ID),str(hour),str(C1),str(banner_pos),str(app_domain),str(app_category),str(device_ip),str(device_model),str(device_type),str(device_conn_type),str(C14),str(C15),str(C16),str(C17),str(C18),str(C19),str(C20),str(C21)))
         if t % 100000 == 0:
             print("%s\t%s"%(t, str(datetime.now() - start)))
             
@@ -89,7 +89,7 @@ test = 'data/test_df_site_smooth.csv'
 
 start = datetime.now()
 with open('data/train_df_site_smooth_ex_id.csv',"wb") as outfile:
-    outfile.write('id,click,hour,C1,banner_pos,site_domain,site_category,device_id,device_ip,device_model,device_type,device_conn_type,C14,C15,C16,C17,C18,C19,C20,C21\n')
+    outfile.write('id,click,hour,C1,banner_pos,site_domain,site_category,device_ip,device_model,device_type,device_conn_type,C14,C15,C16,C17,C18,C19,C20,C21\n')
     for t, row in enumerate(DictReader(open(train))):
         # turn hour really into hour, it was originally YYMMDDHH
         
@@ -100,7 +100,7 @@ with open('data/train_df_site_smooth_ex_id.csv',"wb") as outfile:
         banner_pos = row['banner_pos']
         site_domain = row['site_domain']
         site_category = row['site_category']
-        device_id = row['device_id']
+        #device_id = row['device_id']
         device_ip = row['device_ip']
         device_model = row['device_model']
         device_type = row['device_type']
@@ -115,7 +115,7 @@ with open('data/train_df_site_smooth_ex_id.csv',"wb") as outfile:
         C21 = row['C21']
         
 
-        outfile.write('%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n' % (str(ID), str(click),str(hour),str(C1),str(banner_pos),str(site_domain),str(site_category),str(device_id),str(device_ip),str(device_model),str(device_type),str(device_conn_type),str(C14),str(C15),str(C16),str(C17),str(C18),str(C19),str(C20),str(C21)))
+        outfile.write('%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n' % (str(ID), str(click),str(hour),str(C1),str(banner_pos),str(site_domain),str(site_category),str(device_ip),str(device_model),str(device_type),str(device_conn_type),str(C14),str(C15),str(C16),str(C17),str(C18),str(C19),str(C20),str(C21)))
         if t % 100000 == 0:
             print("%s\t%s"%(t, str(datetime.now() - start)))
         
@@ -123,7 +123,7 @@ with open('data/train_df_site_smooth_ex_id.csv',"wb") as outfile:
  # -- test data -- #
 start = datetime.now()
 with open('data/test_df_site_smooth_ex_id.csv',"wb") as outfile:
-    outfile.write('id,hour,C1,banner_pos,site_domain,site_category,device_id,device_ip,device_model,device_type,device_conn_type,C14,C15,C16,C17,C18,C19,C20,C21\n')
+    outfile.write('id,hour,C1,banner_pos,site_domain,site_category,device_ip,device_model,device_type,device_conn_type,C14,C15,C16,C17,C18,C19,C20,C21\n')
     for t, row in enumerate(DictReader(open(test))):
          
         ID = row['id']
@@ -132,7 +132,7 @@ with open('data/test_df_site_smooth_ex_id.csv',"wb") as outfile:
         banner_pos = row['banner_pos']
         site_domain = row['site_domain']
         site_category = row['site_category']
-        device_id = row['device_id']
+        #device_id = row['device_id']
         device_ip = row['device_ip']
         device_model = row['device_model']
         device_type = row['device_type']
@@ -147,6 +147,6 @@ with open('data/test_df_site_smooth_ex_id.csv',"wb") as outfile:
         C21 = row['C21']
         
             
-        outfile.write('%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n' % (str(ID),str(hour),str(C1),str(banner_pos),str(site_domain),str(site_category),str(device_id),str(device_ip),str(device_model),str(device_type),str(device_conn_type),str(C14),str(C15),str(C16),str(C17),str(C18),str(C19),str(C20),str(C21)))
+        outfile.write('%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n' % (str(ID),str(hour),str(C1),str(banner_pos),str(site_domain),str(site_category),str(device_ip),str(device_model),str(device_type),str(device_conn_type),str(C14),str(C15),str(C16),str(C17),str(C18),str(C19),str(C20),str(C21)))
         if t % 100000 == 0:
             print("%s\t%s"%(t, str(datetime.now() - start)))        
