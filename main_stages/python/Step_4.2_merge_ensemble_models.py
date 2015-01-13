29b27b48d4a8f0a942130e3214e46c32e4e93d90
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-01-14 07:19:22 +1100

    merge

--- main_stages/python/Step_4.2_merge_ensemble_models.py
+++ main_stages/python/Step_4.2_merge_ensemble_models.py
@@ -8,12 +8,17 @@ from datetime import datetime
 from csv import DictReader
 import pandas as pd
 
-pred_file1 = 'submission_py_0.13_1_1_1_ex_not_train.csv'
+pred_file1 = 'submission_py_site_app_0.13_1_1_1_s5.csv'
 pred_file2 = 'submit_xgboost_app_site.csv'  
+pred_file3 = 'submit_libFM_pred.csv'  
 
 pred_2 = {}
 for t, row in enumerate(DictReader(open(pred_file2))): # site/app
     pred_2[row['id']] = row['click']
+
+pred_3 = {}
+for t, row in enumerate(DictReader(open(pred_file3))): # site/app
+    pred_3[row['id']] = row['click']
 
 start = datetime.now()
 with open('submit_ensemble_merge.csv',"wb") as outfile:
@@ -22,7 +27,7 @@ with open('submit_ensemble_merge.csv',"wb") as outfile:
         
         ID = row['id']
         click = row['click']
-        click = 0.5*float(click) + 0.5*float(pred_2[ID])
+        click = 0.4*float(click) + 0.4*float(pred_2[ID]) + 0.2*float(pred_3[ID])
         
         outfile.write('%s,%s\n' % (str(ID), str(click)))
         if t % 100000 == 0:
