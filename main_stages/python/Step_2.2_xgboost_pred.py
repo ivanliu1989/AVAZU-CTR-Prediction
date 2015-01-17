4751648807ae0185a4729613ae876c42721a9acd
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-01-17 13:42:40 +1100

    update

--- main_stages/python/Step_2.2_xgboost_pred.py
+++ main_stages/python/Step_2.2_xgboost_pred.py
@@ -6,13 +6,13 @@ Created on Tue Jan  6 14:46:43 2015
 """
 import pandas as pd
 
-xgboost_app = 'xgboost/libFM_pred_app.txt'
-xgboost_site = 'xgboost/libFM_pred_site.txt'
+xgboost_app = 'xgboost/Pred_app.txt'
+xgboost_site = 'xgboost/Pred_site.txt'
 pred_app = pd.read_csv(xgboost_app,index_col = False, header = None)
 pred_site = pd.read_csv(xgboost_site,index_col = False, header = None)
 
-submit_app = 'data/onehot/test_df_smooth_hash_app.csv'
-submit_site = 'data/onehot/test_df_smooth_hash_site.csv'
+submit_app = 'data/test_df_app_smooth.csv'
+submit_site = 'data/test_df_site_smooth.csv'
 submit_app = pd.read_csv(submit_app)
 submit_site = pd.read_csv(submit_site)
 
@@ -22,4 +22,4 @@ submission_site = pd.DataFrame({'id':submit_site['id'],
                                 'click':pred_site[0]})
 
 submission = submission_app.append(submission_site,ignore_index=True)
-submission.to_csv('xgboost/submit_xgboost_app_site.csv',index=False)
+submission.to_csv('xgboost/submit_xgboost_900_03.csv',index=False)
