ce295fa40a40e91330e9492a124125b60019516e
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-01-22 22:13:47 +1100

    dual log

--- MAIN_IMPROVE/4_csv2vw_positiveClick.py
+++ MAIN_IMPROVE/4_csv2vw_positiveClick.py
@@ -31,9 +31,7 @@ def csv_to_vw(loc_csv, loc_output, train=True):
       if train: #we care about labels
         if row['click'] == '1':
           label = 1
-        else:
-          label = -1 #we set negative label to -1
-        outfile.write( "%s '%s |c %s\n" % (label,row['id'],categorical_features) )
+          outfile.write( "%s '%s |c %s\n" % (label,row['id'],categorical_features) )
 		
       else: #we dont care about labels
         outfile.write( "1 '%s |c %s\n" % (row['id'],categorical_features) )
@@ -45,7 +43,7 @@ def csv_to_vw(loc_csv, loc_output, train=True):
   print("\n %s Task execution time:\n\t%s"%(e, str(datetime.now() - start)))
 
 
-csv_to_vw("data/train_df_site_smooth.csv", "vw/train_df_site.vw",train=True)
-csv_to_vw("data/test_df_site_smooth.csv", "vw/test_df_site.vw",train=False)
-csv_to_vw("data/train_df_app_smooth.csv", "vw/train_df_app.vw",train=True)
-csv_to_vw("data/test_df_app_smooth.csv", "vw/test_df_app.vw",train=False)
+csv_to_vw("data/train_df_site_smooth.csv", "vw/train_df_site_positive.vw",train=True)
+csv_to_vw("data/test_df_site_smooth.csv", "vw/test_df_site_positive.vw",train=False)
+csv_to_vw("data/train_df_app_smooth.csv", "vw/train_df_app_positive.vw",train=True)
+csv_to_vw("data/test_df_app_smooth.csv", "vw/test_df_app_positive.vw",train=False)
