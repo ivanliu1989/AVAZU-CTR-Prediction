33d7b4ff1763949e5047619d7a37f10c078aeb67
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-01-23 22:10:41 +1100

    update

--- MAIN_IMPROVE/5_blending_models.py
+++ MAIN_IMPROVE/5_blending_models.py
@@ -12,8 +12,8 @@ pred_file1 = 'pred/blending/submission_py_0.13_1_1_1_29_0.3931037.csv'
 pred_file2 = 'pred/blending/submission_vw_cubic2_0.3957190.csv'  
 pred_file3 = 'pred/blending/submit_libFM_pred_0.4027643.csv'  
 pred_file4 = 'pred/blending/submit_xgboost_900_03_0.3931970.csv' 
-pred_file5 = 'pred/blending/submission_vw_name_60nn_0.3987734.csv'   
-pred_file6 = 'pred/blending/libLinear_logreg_dual_0.3987665.csv'   
+pred_file5 = 'pred/blending/NN_NN_0.3957782.csv'   
+#pred_file6 = 'pred/blending/libLinear_logreg_dual_0.3987665.csv'   
 
 pred_2 = {}
 for t, row in enumerate(DictReader(open(pred_file2))): # site/app
@@ -30,21 +30,22 @@ for t, row in enumerate(DictReader(open(pred_file4))): # site/app
 pred_5 = {}
 for t, row in enumerate(DictReader(open(pred_file5))): # site/app
     pred_5[row['id']] = row['click']
-    
+'''    
 pred_6 = {}
 for t, row in enumerate(DictReader(open(pred_file6))): # site/app
     pred_6[row['id']] = row['click']
-
+'''
 
 start = datetime.now()
-with open('Blending_models_BM_CU_FM_XG_NN_DUAL.csv',"wb") as outfile:
+with open('Blending_models_BM_CU_FM_XG_NN86.csv',"wb") as outfile:
     outfile.write('id,click\n')
     for t, row in enumerate(DictReader(open(pred_file1))):
         
         ID = row['id']
         click = row['click']
-        
-        click = 6/(1/float(click)+1/float(pred_2[ID])+1/float(pred_3[ID])+1/float(pred_4[ID])+1/float(pred_5[ID])+1/float(pred_6[ID]))
+        #if(float(pred_5[ID])==0):
+        #    pred_5[ID]=0.0000000001
+        click = 5/(1/float(click)+1/float(pred_2[ID])+1/float(pred_3[ID])+1/float(pred_4[ID])+1/float(pred_5[ID]))
         #click = 0.3*(0.7*float(click)+0.3*float(pred_2[ID])) + 0.3*float(pred_3[ID]) + 0.2*float(pred_3[ID]) + 0.2*float(pred_5[ID])
         
         outfile.write('%s,%s\n' % (str(ID), str(click)))
