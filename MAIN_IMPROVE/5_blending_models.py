bcda84f316d1203d9b15755f816cb6332b47c0d6
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-01-24 21:53:43 +1100

    update

--- MAIN_IMPROVE/5_blending_models.py
+++ MAIN_IMPROVE/5_blending_models.py
@@ -8,11 +8,11 @@ from datetime import datetime
 from csv import DictReader
 import pandas as pd
 
-pred_file1 = 'pred/blending/ftrl_5_2.5_2.5_0.3915902.csv'
+pred_file1 = 'pred/blending/ftrl_0.13_1_1_1_29_0.3931037.csv'
 pred_file2 = 'pred/blending/libFM_0.4027643.csv'  
 pred_file3 = 'pred/blending/nn_60_80_0.3957782.csv'  
 pred_file4 = 'pred/blending/xgboost_900_03_0.3931970.csv' 
-#pred_file5 = 'pred/blending/NN_NN_0.3957782.csv'   
+pred_file5 = 'pred/blending/pric_cubic.csv'   
 #pred_file6 = 'pred/blending/libLinear_logreg_dual_0.3987665.csv'   
 
 pred_2 = {}
@@ -26,11 +26,11 @@ for t, row in enumerate(DictReader(open(pred_file3))): # site/app
 pred_4 = {}
 for t, row in enumerate(DictReader(open(pred_file4))): # site/app
     pred_4[row['id']] = row['click']
-'''
+
 pred_5 = {}
 for t, row in enumerate(DictReader(open(pred_file5))): # site/app
     pred_5[row['id']] = row['click']
-    
+'''    
 pred_6 = {}
 for t, row in enumerate(DictReader(open(pred_file6))): # site/app
     pred_6[row['id']] = row['click']
@@ -45,7 +45,7 @@ with open('Blending_models_FTRL_FM_XG_NN86.csv',"wb") as outfile:
         click = row['click']
         #if(float(pred_5[ID])==0):
         #    pred_5[ID]=0.0000000001
-        click = 4/(1/float(click)+1/float(pred_2[ID])+1/float(pred_3[ID])+1/float(pred_4[ID]))#+1/float(pred_5[ID]))
+        click = 5/(1/float(click)+1/float(pred_2[ID])+1/float(pred_3[ID])+1/float(pred_4[ID])+1/float(pred_5[ID]))
         #click = 0.3*(0.7*float(click)+0.3*float(pred_2[ID])) + 0.3*float(pred_3[ID]) + 0.2*float(pred_3[ID]) + 0.2*float(pred_5[ID])
         
         outfile.write('%s,%s\n' % (str(ID), str(click)))
