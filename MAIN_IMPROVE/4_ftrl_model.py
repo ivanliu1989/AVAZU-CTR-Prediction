c4207da6a135b65e4b1d27fba287ae75fd89e368
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-01-26 11:46:54 +1100

    update

--- MAIN_IMPROVE/4_ftrl_model.py
+++ MAIN_IMPROVE/4_ftrl_model.py
@@ -12,23 +12,23 @@ from math import exp, log, sqrt
 # A, paths
 train = 'data/train_df_site_complete.csv'               # path to training file
 test = 'data/test_df_site_complete.csv'                 # path to testing file
-submission = 'ftrl_complete_013_1_1_1_29_1.csv'  # path of to be outputted submission file
+submission = 'ftrl_complete_013_1_4_2_29_1.csv'  # path of to be outputted submission file
 train2 = 'data/train_df_app_complete.csv'               # path to training file
 test2 = 'data/test_df_app_complete.csv'                 # path to testing file
 
 # B, model
 alpha_site = 0.13  # learning rate
 beta_site = 1   # smoothing parameter for adaptive learning rate
-L1_site = 1     # L1 regularization, larger value means more regularized
-L2_site = 1     # L2 regularization, larger value means more regularized
+L1_site = 4     # L1 regularization, larger value means more regularized
+L2_site = 2     # L2 regularization, larger value means more regularized
 
 alpha_app = 0.13  # learning rate
 beta_app = 1   # smoothing parameter for adaptive learning rate
-L1_app = 1     # L1 regularization, larger value means more regularized
-L2_app = 1     # L2 regularization, larger value means more regularized
+L1_app = 4     # L1 regularization, larger value means more regularized
+L2_app = 2     # L2 regularization, larger value means more regularized
 
 # C, feature/hash trick
-D = 2 ** 29             # number of weights to use
+D = 2 ** 28             # number of weights to use
 interaction_app = False     # whether to enable poly2 feature interactions
 interaction_site = False     # whether to enable poly2 feature interactions
 
