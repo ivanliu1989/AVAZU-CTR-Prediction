9e63fa96fb858883811a2caead923f1cdae2a7ef
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-02-01 21:04:02 +1100

    smooth

---
+++ Summary/3_noisy_removal.r
@@ -0,0 +1,48 @@
+setwd('C:/Users/Ivan.Liuyanfeng/Desktop/Data_Mining_Work_Space/VAZU')
+rm(list=ls()); gc()
+require(data.table)
+
+#train_app <- data.frame(fread('other/train_df_app_split.csv',colClasses='character',header=T)) # 14596137
+#test_app <- data.frame(fread('other/test_df_app_split.csv',colClasses='character',header=T)) # 1719304
+
+train_app <- data.frame(fread('other/train_df_site_split.csv',colClasses='character',header=T)) # 25832830
+test_app <- data.frame(fread('other/test_df_site_split.csv',colClasses='character',header=T)) # 2858160
+
+ls();gc()
+test_app$click <- 0
+
+dim(train_app);dim(test_app)
+train_app <- rbind(train_app,test_app)
+head(train_app)
+rm(test_app);gc()
+
+### remove ###
+for (n in 3:length(colnames(train_app))){
+    
+    val_list <- table(train_app[,n])
+    val_list <- which(val_list<=5)
+    names(val_list)
+    filter <- train_app[which(train_app[,n] %in% names(val_list)),]
+    filter <- filter[which(filter$click == '1'),]
+    val_list_op <- table(filter[,n])
+    val_list_final <- names(val_list)[which(!names(val_list) %in% names(val_list_op))]
+    train_app[which(train_app[,n] %in% val_list_final),n] <- 'other'
+   
+    gc()
+    print(paste('Feature',colnames(train_app)[n],'finished.', length(val_list_final), 'features removed!'))
+}
+
+dim(train_app)
+#test_app <- train_app[14596138:16315441 , -2]
+test_app <- train_app[25832831:28690990 , -2]
+head(test_app);gc()
+#train_app <- train_app[-c(14596138:16315441),]
+train_app <- train_app[-c(25832831:28690990),]
+head(train_app);gc()
+dim(train_app);dim(test_app)
+
+write.csv(train_app, file = 'data/train_df_app_split_smooth.csv', quote = F, row.names = F)
+write.csv(test_app, file = 'data/test_df_app_split_smooth.csv', quote = F, row.names = F)
+
+write.csv(train_app, file = 'data/train_df_site_split_smooth.csv', quote = F, row.names = F)
+write.csv(test_app, file = 'data/test_df_site_split_smooth.csv', quote = F, row.names = F)
