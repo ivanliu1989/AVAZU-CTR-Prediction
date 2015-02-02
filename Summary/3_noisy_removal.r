7a9343c5a74750eececcd4dedf430db5f88d9a85
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-02-03 10:30:20 +1100

    smooth

--- Summary/3_noisy_removal.r
+++ Summary/3_noisy_removal.r
@@ -3,18 +3,18 @@ setwd('C:/Users/Ivan.Liuyanfeng/Desktop/Data_Mining_Work_Space/VAZU')
 rm(list=ls()); gc()
 require(data.table)
 
-train_app <- data.frame(fread('other/train_df_app_split.csv',colClasses='character',header=T,na.strings = "")) # 14596137
-test_app <- data.frame(fread('other/test_df_app_split.csv',colClasses='character',header=T,na.strings = "")) # 1719304
+# train_app <- data.frame(fread('other/train_df_app_split.csv',colClasses='character',header=T,na.strings = "")) # 14596137
+# test_app <- data.frame(fread('other/test_df_app_split.csv',colClasses='character',header=T,na.strings = "")) # 1719304
 
-# train_app <- data.frame(fread('other/train_df_site_split.csv',colClasses='character',header=T)) # 25832830
-# test_app <- data.frame(fread('other/test_df_site_split.csv',colClasses='character',header=T)) # 2858160
+train_app <- data.frame(fread('other/train_df_site_split.csv',colClasses='character',header=T)) # 25832830
+test_app <- data.frame(fread('other/test_df_site_split.csv',colClasses='character',header=T)) # 2858160
 
 ls();gc()
 test_app$click <- 0
 
 dim(train_app);dim(test_app)
 train_app <- rbind(train_app,test_app)
-head(train_app)
+#head(train_app)
 rm(test_app);gc()
 
 ### remove ###
@@ -38,16 +38,16 @@ for (n in 3:length(colnames(train_app))){
 }
 
 dim(train_app)
-test_app <- train_app[14596138:16315441 , -2]
-# test_app <- train_app[25832831:28690990 , -2]
+# test_app <- train_app[14596138:16315441 , -2]
+test_app <- train_app[25832831:28690990 , -2]
 tail(test_app);gc()
-train_app <- train_app[-c(14596138:16315441),]
-# train_app <- train_app[-c(25832831:28690990),]
+# train_app <- train_app[-c(14596138:16315441),]
+train_app <- train_app[-c(25832831:28690990),]
 head(train_app);gc()
 dim(train_app);dim(test_app)
 
-write.csv(train_app, file = 'data/train_df_app_split_smooth.csv', quote = F, row.names = F)
-write.csv(test_app, file = 'data/test_df_app_split_smooth.csv', quote = F, row.names = F)
+# write.csv(train_app, file = 'data/train_df_app_split_smooth.csv', quote = F, row.names = F)
+# write.csv(test_app, file = 'data/test_df_app_split_smooth.csv', quote = F, row.names = F)
 
-# write.csv(train_app, file = 'data/train_df_site_split_smooth.csv', quote = F, row.names = F)
-# write.csv(test_app, file = 'data/test_df_site_split_smooth.csv', quote = F, row.names = F)
+write.csv(train_app, file = 'data/train_df_site_split_smooth.csv', quote = F, row.names = F)
+write.csv(test_app, file = 'data/test_df_site_split_smooth.csv', quote = F, row.names = F)
