a856b41e72b19712c0abf9d7346f60de42faf7a3
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-02-05 10:34:31 +1100

    update

--- Summary/3_noisy_removal.r
+++ Summary/3_noisy_removal.r
@@ -3,11 +3,11 @@ setwd('/Users/ivan/Work_directory/VAZU/')
 rm(list=ls()); gc()
 require(data.table)
 
-# train_app <- data.frame(fread('other/train_df_app_split.csv',colClasses='character',header=T,na.strings = "")) # 14596137
-# test_app <- data.frame(fread('other/test_df_app_split.csv',colClasses='character',header=T,na.strings = "")) # 1719304
+train_app <- data.frame(fread('other/train_df_app_split.csv',colClasses='character',header=T,na.strings = "")) # 14596137
+test_app <- data.frame(fread('other/test_df_app_split.csv',colClasses='character',header=T,na.strings = "")) # 1719304
 
-train_app <- data.frame(fread('other/train_df_site_split.csv',colClasses='character',header=T)) # 25832830
-test_app <- data.frame(fread('other/test_df_site_split.csv',colClasses='character',header=T)) # 2858160
+# train_app <- data.frame(fread('other/train_df_site_split.csv',colClasses='character',header=T)) # 25832830
+# test_app <- data.frame(fread('other/test_df_site_split.csv',colClasses='character',header=T)) # 2858160
 
 ls();gc()
 test_app$click <- 0
@@ -21,8 +21,9 @@ rm(test_app);gc()
 for (n in 3:length(colnames(train_app))){
     
     val_list <- table(train_app[,n])
-    if(length(val_list)>=1){
-        val_list <- which(val_list<=5)
+    
+    val_list <- which(val_list<=5)
+    if(length(val_list)>=2){
         #     names(val_list)
 #         filter <- train_app[which(train_app[,n] %in% names(val_list)),]
 #         filter <- filter[which(filter$click == '1'),]
@@ -38,16 +39,16 @@ for (n in 3:length(colnames(train_app))){
 }
 
 dim(train_app)
-# test_app <- train_app[14596138:16315441 , -2]
-test_app <- train_app[25832831:28690990 , -2]
+test_app <- train_app[14596138:16315441 , -2]
+# test_app <- train_app[25832831:28690990 , -2]
 tail(test_app);gc()
-# train_app <- train_app[-c(14596138:16315441),]
-train_app <- train_app[-c(25832831:28690990),]
+train_app <- train_app[-c(14596138:16315441),]
+# train_app <- train_app[-c(25832831:28690990),]
 head(train_app);gc()
 dim(train_app);dim(test_app)
 
-# write.csv(train_app, file = 'data/train_df_app_split_smooth.csv', quote = F, row.names = F)
-# write.csv(test_app, file = 'data/test_df_app_split_smooth.csv', quote = F, row.names = F)
+write.csv(train_app, file = 'data/train_df_app_split_smooth.csv', quote = F, row.names = F)
+write.csv(test_app, file = 'data/test_df_app_split_smooth.csv', quote = F, row.names = F)
 
-write.csv(train_app, file = 'data/train_df_site_split_smooth.csv', quote = F, row.names = F)
-write.csv(test_app, file = 'data/test_df_site_split_smooth.csv', quote = F, row.names = F)
+# write.csv(train_app, file = 'data/train_df_site_split_smooth.csv', quote = F, row.names = F)
+# write.csv(test_app, file = 'data/test_df_site_split_smooth.csv', quote = F, row.names = F)
